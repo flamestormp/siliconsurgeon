832e9a9676b3aca60f1711947a7b25da692879b7
Phillip Nguyen <flamestormp@gmail.com>
2016-11-05 15:17:20 -0400

    First Files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Silicon Surgeon - Home</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Silicon Surgeon Inc.">
+        <meta name="description" content="We provide the best treatement for your electronics!" >
+        <link href="css/theme.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+
+    </body>
+</html>
