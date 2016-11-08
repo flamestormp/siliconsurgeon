579073197bfc86d42d8cf26ea744b62f92f13d83
Phillip Nguyen <flamestormp@gmail.com>
2016-11-08 13:43:32 -0500

    Preliminary work on contacting
    Added cell phone number on navbar (most professional websites have a
    number somewhere that is seen right away, plus there's a bunch of empty
    space that can be filled. Also embedded a google maps iframe pointing to
    my house as the mock location.

--- contact.php
+++ contact.php
@@ -11,6 +11,7 @@
     <body>
         <?php include "top.php"?>
         <p>this is where you can fill out a form to contact us</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.4351803440086!2d-73.11537868455066!3d44.46525180755847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca79b35d290fdd%3A0x1c3cf1c6a6fc0c13!2s1015+Industrial+Ave%2C+Williston%2C+VT+05495!5e0!3m2!1sen!2sus!4v1478630386301" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         <?php include "footer.php" ?>
     </body>
 </html>
