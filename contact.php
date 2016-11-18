0e52c74226fb1682d3dae900c464e175ebddd95d
Phillip Nguyen <flamestormp@gmail.com>
2016-11-18 16:50:28 -0500

    Seperate map and form. Make new page info.php
    I couldn't get the may to display correctly when the page is resized, so
    this was the idea I came up with.We need another page anyways. Info.php
    should contain store hours, address, the map, and other businessy info.

--- contact.php
+++ contact.php
@@ -111,9 +111,9 @@ $mailed = sendMail($to, $cc, $bcc, $from, $subject, $message);
 }
 ?>
         <!-- DISPLAY ERRORS HERE OR SUCESS MESSAGE IF NOT FIRST VISIT -->
+        <h2> Send Us A Message </h2>
         <form action="<?php print $phpSelf; ?>" id="contactUs" method="post">
             <fieldset class="contactform">
-                <div id="contactform">
                 <label for="txtName">Name: </label><br>
                 <input <?php if($nameERROR) print 'class="mistake"'; ?>
                     type="text"
@@ -159,8 +159,6 @@ $mailed = sendMail($to, $cc, $bcc, $from, $subject, $message);
                     name="Send"
                     id="Send"
                     value="Send">
-                </div>
-                <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.4351803440086!2d-73.11537868455066!3d44.46525180755847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca79b35d290fdd%3A0x1c3cf1c6a6fc0c13!2s1015+Industrial+Ave%2C+Williston%2C+VT+05495!5e0!3m2!1sen!2sus!4v1478630386301" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
             </fieldset>
         </form>
         <?php include "footer.php" ?>
--- top.php
+++ top.php
@@ -5,6 +5,7 @@
     <a href="index.php" class="nav">Home</a>
     <a href="services.php" class="nav">Services</a>
     <a href="contact.php" class="nav">Contact</a>
+    <a href="info.php" class="nav">Info</a>
     <a href="about.php" class="nav">About Us</a>
     <p> Call Us! 1-802-555-5555 </p>
 </nav>
