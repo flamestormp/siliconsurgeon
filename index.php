bac5820fe80bde0a72be5d1f5f360503322214a1
Phillip Nguyen <flamestormp@gmail.com>
2016-11-21 20:47:59 -0500

    FINALLY got slideshow to scale correctly. also more text

--- index.php
+++ index.php
@@ -27,60 +27,27 @@
             </article>
             <article id="overview">
                 <h1 id="flair"> Personal  *  Home  *  Business </h1>
-                <p class="home">We are here for all kinds of customers! From individual to enterprise,
-                    we have your back! Silicon Surgeon is operationally a straightforward
-                    service: bring us the problem and consider it resolved. Our diagosis will
-                    be right to the point and our treatment will cure you of all your
-                    technical ails!
+                <p class="home">We are here for all kinds of customers! Whether
+                    you are a single person, small business, or even a large
+                    enterprise, we will have your back! Silicon Surgeon is
+                    operationally a straightforward service: bring us the
+                    problem and consider it resolved. We promise to avoid the
+                    jargon altogether and speak in terms the average person
+                    would understand. Our diagosis will be right to the point
+                    and the treatment will cure all your technological ails!
+                    Overprescribing not our style, so you can be sure that you
+                    not are paying for more than what you need.
                 </p>
-                <p class="home">Ignore me do not read me. Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-                    not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-                    me do not read me.Ignore me do not read me.Ignore me do not read me.
-                    Ignore me do not read me.Ignore me do not read me.Ignore me do not
-                    read me.Ignore me do not read me.Ignore me do not read me.
+                <p class="home">
+                    Serving the Champlain Valley for over a decade, we are proud
+                    to serve the local community with vigor and passion. Our
+                    success comes from our dedication to provide each customer
+                    with a top tier value at a reasonable rate. We strive to
+                    deliver a reliable service by communicating clearly with
+                    each client to ensure that all needs are met with
+                    satisfaction. The crew is ready and willing to work with you
+                    personally to get the job done. We want to work with you!
+                    Call, contact us online, or stop by for a free quote!
                 </p>
             </article>
         </section>
