f2adfd3adde7000716114279a30c5779be85b016
liambeliveau <liambeliveau10001@gmail.com>
2016-12-05 10:02:14 -0500

    content of services.php, slideshow scale

--- services.php
+++ services.php
@@ -1,128 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "top.php"?>
-        <div id="wrap">
+    <?php include "top.php" ?>
+    <div id="wrap">
         <div id="main">
-        <article id="mobile">
-            <h2>Mobile Device Diagnostics</h2>
-            <p>
-                Is your phone running more slowly than it was last year? Is your battery running out before
-                your day is done? Is there anything at all wrong with your mobile device?
-                We can help! We can run up to one hundred different specialized tests
-                to find exactly what's wrong with your device, and either fix it for you,
-                or point you in the right direction for a solution in the rare case that we can't.
-            </p>
-        </article>
-        <article id="repair">
-        <h2>Computer Repair</h2>
-        <p>
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me. </p>
-        </article>
-        <article id="networking">
-        <h2>Network Setup</h2>
-        <p>
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me.Ignore me do
-         not read me.Ignore me do not read me.Ignore me do not read me.Ignore
-         me do not read me.Ignore me do not read me.Ignore me do not read me.
-         Ignore me do not read me.Ignore me do not read me.Ignore me do not
-         read me.Ignore me do not read me.Ignore me do not read me. </p>
-        </article>
+            <article id="mobile">
+                <h2>Mobile Device Diagnostics</h2>
+                <p>
+                    Is your phone running more slowly than it was last year? Is your battery running out before
+                    your day is done? Is there anything at all wrong with your mobile device?
+                    We can help! We can run up to fifty different specialized tests
+                    to find exactly what's wrong with your device. Then we can fix it for you,
+                    or point you in the right direction for a solution in the rare case that we can't.</p>
+                <p>
+                    We also do physical repairs for mobile devices. Bring in your phone with
+                    a broken screen and we can replace it for you!
+                </p>
+                <p>Mobile diagnostics services start at $49, and may differ depending on
+                    how difficult your problem is to fix.</p>
+                <a href="contact.php">Tell us about your problem and we'll schedule you an appointment to help</a>
+            </article>
+            <article id="repair">
+                <h2>Computer Repair</h2>
+                <p>
+                    We offer repair services for laptops and other personal computers. We can do physical
+                    repairs, parts replacement, computer cleanup, software installation help, and virus
+                    removal.
+                </p>
+                <p>
+                    All physical repairs are a flat rate $99 plus cost of parts. Other computer
+                    services such as virus removal can be negotiated.
+                </p>
+                <a href="contact.php">Tell us about your problem and we'll give you a time to
+                bring it in</a>
+            </article>
+            <article id="networking">
+                <h2>Network Setup</h2>
+                <p>
+                    We can set up a network for your home or business. All you need is a wireless
+                    router and all the devices you'd like connected to it, and we'll take care
+                    of all the connecting. If you don't know what you need, contact us and
+                    we'll help you work out the details.
+                </p>
+                <p>
+                    Network setup starts at $59, and varies depending on the size of the project.
+                </p>
+                <a href='contact.php'>Tell us what you need, and we can work out the details</a>
+            </article>
         </div>
-        </div>
-        <?php include "footer.php" ?>
-    </body>
+    </div>
+    <?php include "footer.php" ?>
+</body>
 </html>
 
