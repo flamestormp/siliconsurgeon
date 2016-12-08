dede0357225fec6dccdbaf472070d40e4cb61f84
Phillip Nguyen <flamestormp@gmail.com>
2016-12-08 11:53:19 -0500

    finish about.php

--- about.php
+++ about.php
@@ -7,22 +7,26 @@
             <aside class="about">
             <img class="profile" src="img/phillip.jpg" alt="Phillip Nguyen">
                 <p><strong> Dr. Phillip H. Nguyen </strong></p>
-                <p> He may look young, but his experiences make up a lifetime.
-                    Specializes in forensic data recovery and network traffic
-                    analysis. After a long and sucessful career as a programmer
-                    at several companies, he decided to go back to his roots and
-                    work on repairing consumer electronics. To be continued...
+                <p> He may look young, but the sum of his experiences make up a lifetime.
+                    Specialized in forensic data recovery and network traffic
+                    analysis, but later moved on to study computer science. After a long
+                    and sucessful career as a programmer at several companies, he decided
+                    to go back to his roots and work on repairing consumer electronics.
+                    Phillip always shows passion and dedication in all his endeavors, but
+                    Silicon Surgeon is only the next step in his grand mission.
                 </p>
             </aside>
             <br>
             <aside class="about">
             <img class="profile" src="img/liam.jpg" alt="Liam Beliveau">
                 <p><strong> Prof. Liam Beliveau </strong></p>
-                <p> He may look young, but his experiences make up a lifetime.
-                    Specializes in forensic data recovery and network traffic
-                    analysis. After a long and sucessful career as a programmer
-                    at several companies, he decided to go back to his roots and
-                    work on repairing consumer electronics. To be continued...
+                <p> A keen eye and a sharp intuition makes this fellow the enemy of all bugs.
+                    His gut is second to none when it comes to the art of problem solving.
+                    After teaching Informational Technology and Computer Science at the
+                    University of Vermont for over a decade, he decided that the classroom
+                    wasn't where he wanted to be. He was always the curious type, disassembling
+                    computers since the early age of four! Silicon Surgeon exists today because
+                    his sixth sense did him well during a trip to Vegas.
                 </p>
             </aside>
         </div>
