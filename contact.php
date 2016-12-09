fbf8e36727cd756b60240df2db5c9a164b6e407c
liambeliveau <liambeliveau10001@gmail.com>
2016-12-08 22:26:54 -0500

    documentation!

--- contact.php
+++ contact.php
@@ -49,6 +49,7 @@
                 <p><b>Fax: </b>+1-408-999 8888</p>
                 <p><b>Address: </b>1015 Industrial Avenue, Williston, VT 05495</p>
             </div>
+            <!--iframe with map to show our address-->
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.4351803440086!2d-73.11537868455066!3d44.46525180755847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca79b35d290fdd%3A0x1c3cf1c6a6fc0c13!2s1015+Industrial+Ave%2C+Williston%2C+VT+05495!5e0!3m2!1sen!2sus!4v1478630386301"
                     width="500" height="450" style="border:0" allowfullscreen>
             </iframe>
--- testimonials.php
+++ testimonials.php
@@ -10,12 +10,15 @@
                 <br/>
                 <hr/>
                 <?php
+                //open csv with list of testimonials
                 $myFile = fopen("data/testimonials.csv", "r") or die("Unable to open file!");
+                //store testimonials in a list
                 while (!feof($myFile)) {
                     $list[] = fgetcsv($myFile);
                 }
                 fclose($myFile);
                 print "\n";
+                //iterate through the list and print each testimonial
                 foreach ($list as $entry) {
                     if ($entry[1] == "") {
                         continue; // for the off chance we get a blank entry
@@ -26,6 +29,7 @@
                     print "\t\t\t<hr/>\n";
                 }
 
+                //initialize form variables (for submitting testimonial)
                 $thisURL = $domain . $phpSelf;
                 $txtReview = "";
                 $txtReviewERROR = false;
@@ -34,32 +38,37 @@
                 $errorMsg = array();
                 $dataRecord = array();
 
+                //for when form is submitted
                 if (isset($_POST["submit"])) {
+                    //security check
                     if (!securityCheck($thisURL)) {
                         $msg = "<p> Sorry you cannot view this page.";
                         $msg .= "This incident will be reported.</p>";
                         die($msg);
                     }
-
+                    //save form entries to $dataRecord list
                     $txtReview = htmlentities($_POST["txtReview"], ENT_QUOTES, "UTF-8");
                     $dataRecord[] = $txtReview;
 
                     $txtName = htmlentities($_POST["txtName"], ENT_QUOTES, "UTF-8");
                     $dataRecord[] = $txtName;
 
+                    //save $dataRecord to csv
                     if (!$errorMsg) {
                         $filename = "data/testimonialSubmits.csv";
                         $file = fopen($filename, 'a');
                         fputcsv($file, $dataRecord);
                         fclose($file);
                     }
+                    //hide form and thank user for submission
                     if (isset($_POST["submit"]) AND empty($errorMsg)) {
                         print "<h2 id='sendReview'>Thank you for your submission!</h2>";
                     }
                 } else {
+                    //show form
                     print "<h2>Send us a review!</h2>";
                     ?>
-                    <form action="<?php print $phpSelf; ?>#sendReview" id="sendReview" method="post">
+                    <form action="<?php print $phpSelf;//form directs to same spot on the page?>#sendReview" id="sendReview" method="post">
                         <fieldset>
                             <textarea name="txtReview" id="message" cols="50" rows="20"></textarea><br>
                             <label for="txtName">
